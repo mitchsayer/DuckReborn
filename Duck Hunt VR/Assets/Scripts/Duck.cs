f3d26007d1e99a274ded4aaecb6282d2459e90a9
gtbeazley <50130971+gtbeazley@users.noreply.github.com>
2020-10-12 06:44:47 +1100

    Duck spawner and duck movement errrors

---
+++ Duck Hunt VR/Assets/Scripts/Duck.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+
+    private float m_speed;
+    public float Speed { get { return m_speed; } set { m_speed = value; } }
+
+    private float m_reachingDistance;
+    public float ReachingDistance {  get { return m_reachingDistance; } set { m_reachingDistance = value; } }
+
+    public Vector3[] _path;
+
+    private Vector3 m_moveTarget;
+    private int m_moveTarIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move if there is a target and a speed set
+        if (m_moveTarget != new Vector3(0, 0, 0) && m_speed != 0)
+            transform.position += m_speed * (m_moveTarget - transform.position) * Time.deltaTime;
+
+     
+
+        if (_path.Length > 0)
+        { 
+            //If duck has reached the target with in a reaching distance
+        
+            if (Vector3.Distance(m_moveTarget, transform.position) < m_reachingDistance)
+        
+            {
+              //..And duck has not found the end
+                if (m_moveTarIndex < _path.Length - 1)
+                    //Go to the next target
+                    m_moveTarIndex++;
+                //else
+                    //..otherwise destroy the duck
+                  //  Destroy(this);
+
+            }
+
+            m_moveTarget = _path[m_moveTarIndex];
+        }
+    }
+}
---
+++ Duck Hunt VR/Assets/Scripts/DuckSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSpawner : MonoBehaviour
+{
+    public Duck m_prefab;
+    public float m_speed;
+    public float m_spawnInterval;
+    public float m_reachingDistance;
+    public Vector3[] m_path;
+
+    private float m_timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_timer <= 0)
+        {
+            SpawnDuck();
+            m_timer = m_spawnInterval;
+        }
+        else
+        {
+            m_timer -= Time.deltaTime;
+        }
+    }
+
+    void SpawnDuck()
+    {
+        Duck l_duck = Instantiate(m_prefab, new Vector3(0, 0, 0), new Quaternion(), transform);
+        l_duck.Speed = m_speed;
+            l_duck._path = m_path;
+        for(int i = 0; i < m_path.Length; i++)
+        {
+            l_duck._path[i] =  m_path[i] + transform.position; 
+
+        }
+        l_duck.ReachingDistance = m_reachingDistance;
+    }
+}
